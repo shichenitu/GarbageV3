b0a7a1a89106199a59aec389417ad0e7e3cacde2
shichenitu <shichenitu@163.com>
2026-03-07 13:32:27 +0100

    ui/features/recycling/RecyclingViewModel.kt

--- app/src/main/java/dk/chen/garbagev1/ui/features/recycling/RecyclingViewModel.kt
+++ app/src/main/java/dk/chen/garbagev1/ui/features/recycling/RecyclingViewModel.kt
@@ -1,4 +1,52 @@
 package dk.chen.garbagev1.ui.features.recycling
 
-class RecyclingViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dk.chen.garbagev1.domain.Bin
+import dk.chen.garbagev1.domain.BinRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.annotation.concurrent.Immutable
+import javax.inject.Inject
+
+@HiltViewModel
+class RecyclingViewModel @Inject constructor(
+    private val binRepository: BinRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(value = UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            binRepository.getBins().collect { bins ->
+                _uiState.update { it.copy(bins = bins) }
+            }
+        }
+    }
+
+    val uiEvents: UiEvents = object : UiEvents {
+        override fun onBinSelected(bin: Bin) {
+            _uiState.update { it.copy(selectedBin = bin) }
+        }
+
+        override fun onDismissBinDetails() {
+            _uiState.update { it.copy(selectedBin = null) }
+        }
+    }
+
+    data class UiState(
+        val bins: List<Bin> = emptyList(),
+        val selectedBin: Bin? = null
+    )
+
+    @Immutable
+    interface UiEvents {
+        fun onBinSelected(bin: Bin)
+        fun onDismissBinDetails()
+    }
 }
