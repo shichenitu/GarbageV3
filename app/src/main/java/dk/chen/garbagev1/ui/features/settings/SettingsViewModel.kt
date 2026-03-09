1fae3c8178d79d07355f2a223df7c4ca670bb22e
shichenitu <shichenitu@163.com>
2026-03-09 19:00:51 +0100

    ui/features/settings/SettingsViewModel.kt

---
+++ app/src/main/java/dk/chen/garbagev1/ui/features/settings/SettingsViewModel.kt
@@ -0,0 +1,46 @@
+package dk.chen.garbagev1.ui.features.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dk.chen.garbagev1.domain.Theme
+import dk.chen.garbagev1.domain.UserPreferencesRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.annotation.concurrent.Immutable
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
+
+    val uiState: StateFlow<UiState> = userPreferencesRepository.theme
+        .map { UiState(it) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+            initialValue = UiState()
+        )
+
+    val uiEvents: UiEvents = object : UiEvents {
+        override fun onSetTheme(theme: Theme) {
+            // TODO: Save the theme to the DataStore
+            viewModelScope.launch {
+                userPreferencesRepository.setTheme(theme)
+            }
+        }
+    }
+
+    data class UiState(
+        val theme: Theme = Theme.SYSTEM
+    )
+
+    @Immutable
+    interface UiEvents {
+        fun onSetTheme(theme: Theme)
+    }
+}
