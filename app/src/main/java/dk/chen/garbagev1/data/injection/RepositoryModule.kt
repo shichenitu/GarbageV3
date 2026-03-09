be058ac86d75933d93a5f1ee62f6ad9049f40d16
shichenitu <shichenitu@163.com>
2026-03-09 18:43:13 +0100

    data/injection/RepositoryModule.kt new

--- app/src/main/java/dk/chen/garbagev1/data/injection/RepositoryModule.kt
+++ app/src/main/java/dk/chen/garbagev1/data/injection/RepositoryModule.kt
@@ -1,4 +1,30 @@
 package dk.chen.garbagev1.data.injection
 
-class RepositoryModule {
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dk.chen.garbagev1.data.ItemRepositoryImpl
+import dk.chen.garbagev1.data.BinRepositoryImpl
+import dk.chen.garbagev1.data.UserPreferencesRepositoryImpl
+import dk.chen.garbagev1.domain.ItemRepository
+import dk.chen.garbagev1.domain.BinRepository
+import dk.chen.garbagev1.domain.UserPreferencesRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindItemRepository(impl: ItemRepositoryImpl): ItemRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindBinRepository(impl: BinRepositoryImpl): BinRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindUserPreferencesRepository(impl: UserPreferencesRepositoryImpl): UserPreferencesRepository
 }
