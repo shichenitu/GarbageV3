87371a952f388e1fe5999677bbd2693e218ccafa
shichenitu <shichenitu@163.com>
2026-03-21 17:18:38 +0100

    ui/features/garbage/AffaldKbhScreen.kt*

---
+++ app/src/main/java/dk/chen/garbagev1/ui/features/garbage/AffaldKbhScreen.kt
@@ -0,0 +1,32 @@
+package dk.chen.garbagev1.ui.features.garbage
+
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun AffaldKbhScreen(
+    modifier: Modifier = Modifier
+) {
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+
+                webViewClient = WebViewClient()
+
+                settings.javaScriptEnabled = true
+
+                loadUrl("https://affald.kk.dk/")
+            }
+        }
+    )
+}
