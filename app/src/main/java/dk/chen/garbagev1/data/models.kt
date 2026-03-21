448050d6ba461be7d1b4a8ba6828e9efc850a399
shichenitu <shichenitu@163.com>
2026-03-21 19:39:45 +0100

    data/models.kt & domain/models.kt

--- app/src/main/java/dk/chen/garbagev1/data/models.kt
+++ app/src/main/java/dk/chen/garbagev1/data/models.kt
@@ -7,17 +7,44 @@ import java.util.UUID
 import androidx.core.graphics.toColorInt
 import dk.chen.garbagev1.data.database.ItemEntity
 import dk.chen.garbagev1.data.database.BinEntity
+import dk.chen.garbagev1.domain.RecyclingStation
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ItemDto(
     val id: String = UUID.randomUUID().toString(),
     val what: String,
     val where: String
 )
 
+@Serializable
 data class BinDto(
     val name: String,
     val imageUrl: String,
     val binColor: String
+)
+
+@Serializable
+data class RecyclingStationDto(
+    val id: String,
+    val name: String,
+    val category: String,
+    val address: String,
+    val status: String,
+    val bins: List<String>,
+    val latitude: Double,
+    val longitude: Double
+)
+
+fun RecyclingStationDto.toDomain(): RecyclingStation = RecyclingStation(
+    id = this.id,
+    name = this.name,
+    category = this.category,
+    address = this.address,
+    status = this.status,
+    bins = this.bins,
+    latitude = this.latitude,
+    longitude = this.longitude
 )
 
 fun BinDto.toBin(): Bin = Bin(
