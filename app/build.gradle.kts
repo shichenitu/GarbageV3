995bac5aae3247d3492eecd1b0691dbb4fd04f3b
shichenitu <shichenitu@163.com>
2026-04-03 18:50:49 +0200

    setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,4 +83,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("androidx.compose.material:material-icons-extended:1.7.0")
+    implementation(libs.play.services.location)
 }
